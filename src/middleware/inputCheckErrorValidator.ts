71c148a1d3b0c21a339b89f58b3d5be72498aef1
Chaslen2101 <Chaslen2101@gmail.com>
2024-08-28 23:32:59 +0300

    test_edit_04

--- src/middleware/inputCheckErrorValidator.ts
+++ src/middleware/inputCheckErrorValidator.ts
@@ -6,7 +6,7 @@ import {Request, Response, NextFunction} from "express";
 export const inputErrorCheckValidator = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        const errorsArray = errors.array({onlyFirstError: true}) as { path: string, msg: string }[]
+        const errorsArray = errors.array() as { path: string, msg: string }[]
         const filteredArray:{path: string, msg: string}[] = []
         for (let errorObject of errorsArray) {
             if (filteredArray.find(error => error.path === errorObject.path)) {
