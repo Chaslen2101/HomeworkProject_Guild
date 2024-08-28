4e3d3631ac1dd18b7a13edbcc041af5eacf64b96
Chaslen2101 <Chaslen2101@gmail.com>
2024-08-29 00:28:18 +0300

    test_edit_08

--- src/middleware/post_Middleware/posts_validator.ts
+++ src/middleware/post_Middleware/posts_validator.ts
@@ -5,13 +5,13 @@ export const inputPostsValidation = () => {
     return [
         body("title").trim().exists().withMessage("title is required")
             .isString().withMessage("title should be string")
-            .isLength({max: 30}).withMessage("title should be less then 30 symbols"),
+            .isLength({min:1,max: 30}).withMessage("title should be from 1 to 30 symbols"),
         body("shortDescription").trim().exists().withMessage("shortDescription is required")
             .isString().withMessage("shortDescription should be string")
-            .isLength({max: 100}).withMessage("shortDescription should be less then 100 symbols"),
+            .isLength({min:1,max: 100}).withMessage("shortDescription should be from 1 to 100 symbols"),
         body("content").trim().exists().withMessage("content is required")
             .isString().withMessage("content should be string")
-            .isLength({max: 1000}).withMessage("content max length is 1000 symbols"),
+            .isLength({min:1,max: 1000}).withMessage("content should be from 1 to 1000 symbols"),
         body("blogId").trim().exists().withMessage("blogId is required")
             .isString().withMessage("blogId should be string")
             .custom(inputBlogId => {
