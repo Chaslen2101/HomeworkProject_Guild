25017eb4b79738b75f17c0352974b42a74bde181
Chaslen2101 <Chaslen2101@gmail.com>
2024-10-30 20:40:41 +0300

    homework_09_03

--- src/Features/globalFeatures/apiRequestInfo.ts
+++ src/Features/globalFeatures/apiRequestInfo.ts
@@ -12,11 +12,17 @@ export const RequestInfoCollector = async (req: Request, res: Response, next: Ne
 
     const numberOfRequests = await apiRequestInfoRepository.findRequestInfo(req.ip, req.originalUrl)
 
+    console.log("numberOfRequests: ", numberOfRequests)
+
     if (numberOfRequests.length > 5) {
 
         const earliestRequest = min(numberOfRequests)
         const latestRequest = max(numberOfRequests)
         const difference = differenceInMilliseconds(earliestRequest, latestRequest)
+
+        console.log("earliestRequest: ", earliestRequest)
+        console.log("latestRequest: ", latestRequest)
+        console.log("difference: ", difference)
 
         if (difference > 10000) {
             await apiRequestInfoRepository.deleteRequestInfo(req.ip, req.originalUrl, date)
